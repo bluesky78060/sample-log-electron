f45e9a854a1d917f15c3496c7ba6ae9481f2f0ed
lee chanhee <leechanhee@bluesky78ui-MacBookAir.local>
2025-12-11 16:43:37 +0900

    revert: ZIP 배포 제거, exe만 유지

--- forge.config.js
+++ forge.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'win32'],
+      platforms: ['darwin'],
     },
     {
       name: '@electron-forge/maker-deb',
